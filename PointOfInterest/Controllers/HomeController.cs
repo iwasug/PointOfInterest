7d02b1ceda6ae3d97d287f88811d0b48cb3f9cab
Iwa Sugriwa <iwa.sugriwa@sangkuriang.co.id>
2019-01-19 23:44:12 +0700

    Add jQueryDatatables

--- PointOfInterest/Controllers/HomeController.cs
+++ PointOfInterest/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace PointOfInterest.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Point of Interest";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "";
 
             return View();
         }
