f5d15145cb746866c2536be3e547cefe0eee3ac4
Iwa Sugriwa <iwa.sugriwa@sangkuriang.co.id>
2019-01-19 11:13:00 +0700

    Init Project

---
+++ PointOfInterest/Models/Rumah.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PointOfInterest.Models
+{
+    public class Rumah
+    {
+        public Guid Id { set; get; }
+        public string Nama { set; get; }
+        public string Type { set; get; }
+        public string Alamat { set; get; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTime Create { set; get; }
+        public DateTime Update { set; get; }
+    }
+}
