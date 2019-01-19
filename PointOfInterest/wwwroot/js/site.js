69c9c123c6bc9ad891d8baf1eedb478091536805
Iwa Sugriwa <iwa.sugriwa@sangkuriang.co.id>
2019-01-19 22:38:39 +0700

    Break Point dulu liat MU

--- PointOfInterest/wwwroot/js/site.js
+++ PointOfInterest/wwwroot/js/site.js
@@ -2,3 +2,5 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+var map = null;
+var KeyAPI = "ArElXif6FemUk7MiLv0bV5cZiTcWSO9mkz8JgM4ahhwje3v6Ns022XuXrffbtRJG";
